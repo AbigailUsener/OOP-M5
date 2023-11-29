6b8a8b5a6c1ee147f78e3f084490fa966685d190
Abigail Usener <ausener24@basecampcodingacademy.org>
2023-11-29 10:30:48 -0600

    finished

--- modules/command-line-crud-app/app/models.py
+++ modules/command-line-crud-app/app/models.py
@@ -1,40 +1,34 @@
 from django.db import models
 
 
-class Games(models.Model):
+class Movie(models.Model):
     name = models.TextField()
     release_year = models.IntegerField()
     main_genre = models.TextField()
-    developer = models.TextField()
-    main_character = models.TextField()
 
-def add_game(name,release_date,genres,dev,main_character):
-    game = Games(name=name,release_year=release_date,main_genre=genres,developer=dev,main_character=main_character)
-    game.save()
-    return game
+def add_movie(name,release_date,genres):
+    movie = Movies(name=name,release_year=release_date,main_genre=genres)
+    movie.save()
+    return movie
 
 def view_All():
-    game = Games.objects.all()
-    return game
+    movie = Movies.objects.all()
+    return movie
 
-def view_by_publisher(dev):
-    game = Games.objects.filter(developer=dev)
-    return game
-
-def view_by_game_name(game_name):
+def view_by_movie_name(movie_name):
     try:
-        game = Games.objects.get(name=game_name)
-        return game
+        movie = Movies.objects.get(name=movie_name)
+        return movie
     except:
         return None
 
-def update_publisher(game_name, new_publisher):
-    game = Games.objects.get(name=game_name)
-    game.developer = new_publisher
-    game.save()
-    return game
+def update_release_year(movie_name, new_year):
+    movie = Movies.objects.get(name=movie_name)
+    movie.release_year = new_year
+    movie.save()
+    return movie
 
-def delete_game(game_name):
-    game = Games.objects.get(name=game_name)
-    game.delete()
-    return game
+def delete_movie(movie_name):
+    movie = Movies.objects.get(name=movie_name)
+    movie.delete()
+    return movie
